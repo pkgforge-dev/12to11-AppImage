cb1a0b450bc01b11fe9bbc203469febee2c91c27
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-06 10:05:25 -0400

    build app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	gawk         \
+	imake        \
+	libdrm       \
+	libglvnd     \
+	libx11       \
+	libxcb       \
+	libxi        \
+	libxkbfile   \
+	libxpresent  \
+	libxrandr    \
+	libxshmfence \
+	pixman       \
+	wayland
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -15,4 +28,15 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
-# If the application needs to be manually built that has to be done down here
+git clone --depth 1 https://git.linuxping.win/12to11/12to11 ./12to11
+cd ./12to11
+# fix transparency issue with EGL renderer
+# this will break compat with nvidia prop driver unfortunately
+sed -i \
+	-e 's|egl_config_attribs[1] = 32|egl_config_attribs[1] = 24|' \
+	-e 's|egl_config_attribs[9] = 8|egl_config_attribs[9] = 0|'   \
+	./egl.c
+xmkmf
+make
+install -Dm755 12to11 /usr/bin/12to11
+git rev-parse --short HEAD > ~/version
