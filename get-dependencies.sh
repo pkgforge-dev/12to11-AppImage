27f52cd2228be3cf17a82fd98107f2a380dc6848
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-07-30 14:28:32 -0400

    better fork is now available

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -26,17 +26,4 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-git clone --depth 1 https://git.linuxping.win/12to11/12to11 ./12to11
-cd ./12to11
-# fix transparency issue with EGL renderer
-# this will break compat with nvidia prop driver unfortunately
-sed -i \
-	-e 's|egl_config_attribs\[1\] = 24|egl_config_attribs\[1\] = 32|' \
-	-e 's|egl_config_attribs\[9\] = 0|egl_config_attribs\[9\] = 8|'   \
-	./egl.c
-xmkmf
-make -j"$(nproc)"
-install -Dm755 12to11 /usr/bin/12to11
-git rev-parse --short HEAD > ~/version
+make-aur-package 12to11-git
